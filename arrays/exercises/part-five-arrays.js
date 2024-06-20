118d7d94ce88481f2ace507bdb29c5624c9b3447
Erica Valera <ericavalera@MacBook-Pro.lan>
2024-06-19 22:26:42 -0400

    Array play

--- arrays/exercises/part-five-arrays.js
+++ arrays/exercises/part-five-arrays.js
@@ -2,10 +2,27 @@ let str = 'In space, no one can hear you code.';
 let arr = ['B', 'n', 'n', 5];
 
 //1) Use the split method on the string to identify the purpose of the parameter inside the ().
-
+console.log(str.split());
+//prints the same
+console.log(str.split('e'));
+//removes the letter e from each word
+console.log(str.split(''));
+//isolates each character in parenthesis, for example 'I' 'n' 's' 'p' 'a' 'c' 'e'
+console.log(str.split(' '));
+//seperates every word
 //2) Use the join method on the array to identify the purpose of the parameter inside the ().
-
+console.log(arr.join());
+//prints B,n,n,5
+console.log(arr.join('a'));
+//prints Banana5
+console.log(arr.join(' '));
+//prints B n n 5
+console.log(arr.join(''));
+//prints Bnn5
 //3) Do split or join change the original string/array?
-
+console.log(str);
+console.log(arr);
+//The originals remain unchanged.
 //4) We can take a comma-separated string and convert it into a modifiable array. Try it! Alphabetize the cargoHold string, and then combine the contents into a new string.
 let cargoHold = "water,space suits,food,plasma sword,batteries";
+console.log(cargoHold.split(',').sort().join(','));
--- arrays/exercises/part-six-arrays.js
+++ arrays/exercises/part-six-arrays.js
@@ -1,11 +1,27 @@
 //Arrays can hold different data types, even other arrays! A multi-dimensional array is one with entries that are themselves arrays.
 
 //1) Define and initialize the arrays specified in the exercise to hold the name, chemical symbol and mass for different elements.
-
-//2) Define the array 'table', and use 'push' to add each of the element arrays to it. Print 'table' to see its structure.
+let element1 = ['hydrogen', 'H', 1.008];
+let element2 = ['helium', 'He', 4.003];
+let element26 = ['iron', 'Fe', 55.85];
+//2) Define the array 'table')
+let table= [];
+table.push(element1);
+table.push(element2);
+table.push(element26);
+console.log(table);
 
 //3) Use bracket notation to examine the difference between printing 'table' with one index vs. two indices (table[][]).
-
+console.log(table[1], table[1][1]);
 //4) Using bracket notation and the table array, print the mass of element1, the name for element 2 and the symbol for element26.
+console.log(table[0][0], table[1][2], table[2][1]);
 
 //5) 'table' is an example of a 2-dimensional array. The first “level” contains the element arrays, and the second level holds the name/symbol/mass values. Experiment! Create a 3-dimensional array and print out one entry from each level in the array.
+let arr1= ['blue','green','red'];
+let arr2=['gemini', 'aquarius','libra'];
+let arr3=[70, 15, 1002];
+let tbl=[];
+tbl.push(arr1);
+tbl.push(arr2);
+tbl.push(arr3);
+console.log(tbl[0][1],tbl[1][0],tbl[2][2]);
