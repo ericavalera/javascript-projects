9cf3667ff16d67d510244a6a2bde16b37653b786
Erica Valera <ericavalera@MacBook-Pro.lan>
2024-08-12 21:21:48 -0400

    astronauts

--- fetch/studio/script.js
+++ fetch/studio/script.js
@@ -1,1 +1,31 @@
 //TODO: Add Your Code Below
+window.addEventListener ("load", function (){
+
+    async function astronauts(){
+        let response= await fetch('https://handlers.education.launchcode.org/static/astronauts.json');
+        let data = await response.json();
+        const div = document.getElementById('container');
+
+        for(let i =0; i< data.length; i++){
+            let astroData= data[i];
+
+            div.innerHTML +=`<div class="astronaut">
+            <div class="bio">
+                <h3>${astroData.firstName} ${astroData.lastName} </h3>
+                <ul>
+                <li>${astroData.hoursInSpace}</li>
+                <li>${astroData.active}</li>
+                <li>${astroData.skills}</li>
+                </ul>
+            </div>
+            <img class="avatar" src="${astroData.picture}">
+        </div>`
+        }
+       
+    
+           
+
+        } 
+        astronauts();
+    });
+    
